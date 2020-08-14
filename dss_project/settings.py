ab4604b283d2f99d037fe97393f593f36dad4f0a
maulana ibrahim <maulana020716@gmail.com>
2020-08-14 13:48:21 +0700

    Initial commit

--- dss_project/settings.py
+++ dss_project/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fontawesome',
     'bootstrap4',
     'blog',
     'accounts',
