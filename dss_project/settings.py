b21ecd10fca9aa1527090d1a29d5c2f2dbf65a35
maulana ibrahim <maulana020716@gmail.com>
2020-08-15 13:36:30 +0700

    change to psql

--- dss_project/settings.py
+++ dss_project/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'dss_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangodb',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
