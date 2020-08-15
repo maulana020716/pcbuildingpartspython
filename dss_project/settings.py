5710930abf0af50c7cbecc131ff89807b2eb2762
maulana020716 <maulana020716@gmail.com>
2020-08-15 12:45:49 +0700

    first commit

--- dss_project/settings.py
+++ dss_project/settings.py
@@ -82,23 +82,10 @@ WSGI_APPLICATION = 'dss_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'djangodb',
-
-        'USER': 'postgres',
-
-        'PASSWORD': '12345',
-
-        'HOST': 'localhost',
-
-        'PORT': '',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
 
 
